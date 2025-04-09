6624c9488b4cd1ec1562f5858ea70819f9832998
Mila-7553 <milagrostamara83@gmail.com>
2025-04-09 05:41:15 -0400

    added paths to try to fix failing running issue
    imported and a line to be able to access pockete_classes easier

--- Test/test_plugins.py
+++ Test/test_plugins.py
@@ -1,4 +1,6 @@
 import importlib.metadata
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from pokete_classes.mods import ModError
 import variables as var
 
